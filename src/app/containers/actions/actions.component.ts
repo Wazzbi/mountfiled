18319cd9555161cbc1727b073808a4b0729c5647
Novotný David <danovotny@trask.cz>
2020-10-07 16:36:00 +0200

    kontakty přidány

--- src/app/containers/actions/actions.component.ts
+++ src/app/containers/actions/actions.component.ts
@@ -10,9 +10,21 @@ export class ActionsComponent implements OnInit {
   constructor() { }
 
   articles: any[] = [
-    {imgUrl: 'https://images.unsplash.com/photo-1571366343168-631c5bcca7a4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1051&q=80', title: 'Title 1', text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore corrupti ducimus numquam, autem molestiae repellat, eaque at voluptatibus alias dolorum quibusdam excepturi, consectetur sed earum obcaecati saepe eius. Accusantium ipsum at pariatur nesciunt possimus, corporis illo assumenda? Molestias autem doloremque, magni provident voluptates amet itaque enim sit, rerum dicta nobis necessitatibus dolorum dolorem natus sequi doloribus eaque veritatis ratione? Dignissimos.'},
-    {imgUrl: 'https://images.unsplash.com/photo-1571208756906-92fea1cc1087?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1051&q=80', title: 'Title 2', text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore corrupti ducimus numquam, autem mione? Dignissimos.'},
-    {imgUrl: 'https://images.unsplash.com/photo-1469474968028-56623f02e42e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1053&q=80', title: 'Title 3', text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore corruptearum obcaecati saepe eius. Accusantium ipsum at pariatur nesciunt possimus, corporis illo assumenda? Molestias aubus dolorum dolorem natus sequi doloribusr sit amet consectetur adipisicing elit. Labore corrupti ducimus numquam, autem molestiae repellat, eaque at voluptatibus alias dolorum quibusdam excepturi, consectetur sed earum obcaecati saepe eius. Accusantium ipsum at pariatur nesciuitatibus dolorum dolorem natus sequi doloribus eaque veritatis ratione? Dignissimos.'}
+    {
+      imgUrl: 'https://images.unsplash.com/photo-1571366343168-631c5bcca7a4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1051&q=80',
+      title: 'Title 1',
+      text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore corrupti ducimus numquam, autem molestiae repellat, eaque at voluptatibus alias dolorum quibusdam excepturi, consectetur sed earum obcaecati saepe eius. Accusantium ipsum at pariatur nesciunt possimus, corporis illo assumenda? Molestias autem doloremque, magni provident voluptates amet itaque enim sit, rerum dicta nobis necessitatibus dolorum dolorem natus sequi doloribus eaque veritatis ratione? Dignissimos.'
+    },
+    {
+      imgUrl: 'https://images.unsplash.com/photo-1571208756906-92fea1cc1087?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1051&q=80',
+      title: 'Title 2',
+      text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore corrupti ducimus numquam, autem mione? Dignissimos.'
+    },
+    {
+      imgUrl: 'https://images.unsplash.com/photo-1469474968028-56623f02e42e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1053&q=80',
+      title: 'Title 3',
+      text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore corruptearum obcaecati saepe eius. Accusantium ipsum at pariatur nesciunt possimus, corporis illo assumenda? Molestias aubus dolorum dolorem natus sequi doloribusr sit amet consectetur adipisicing elit. Labore corrupti ducimus numquam, autem molestiae repellat, eaque at voluptatibus alias dolorum quibusdam excepturi, consectetur sed earum obcaecati saepe eius. Accusantium ipsum at pariatur nesciuitatibus dolorum dolorem natus sequi doloribus eaque veritatis ratione? Dignissimos.'
+    }
   ];
 
   ngOnInit() {
--- src/app/containers/contacts/contacts.component.ts
+++ src/app/containers/contacts/contacts.component.ts
@@ -9,6 +9,27 @@ export class ContactsComponent implements OnInit {
 
   constructor() { }
 
+  persons: any[] = [
+    {
+      photo: 'https://images.unsplash.com/photo-1571366343168-631c5bcca7a4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1051&q=80',
+      name: 'Peter Parker',
+      email: 'spidee@lol.cy',
+      tel: '723 568 145'
+    },
+    {
+      photo: 'https://images.unsplash.com/photo-1571208756906-92fea1cc1087?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1051&q=80',
+      name: 'Adam Simson',
+      email: 'guelet@famous.de',
+      tel: '729 569 436'
+    },
+    {
+      photo: 'https://images.unsplash.com/photo-1469474968028-56623f02e42e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1053&q=80',
+      name: 'Susan Hallen',
+      email: 'suzz@qmail.com',
+      tel: '723 568 999'
+    }
+  ];
+
   ngOnInit() {
   }
 

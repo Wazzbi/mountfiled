4dcb135072387cd01469a8a73bb942f0531ec00c
Novotný David <danovotny@trask.cz>
2020-10-07 13:24:34 +0200

    navbar ready

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,15 +1,11 @@
 import { NgModule} from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { FlexLayoutModule } from '@angular/flex-layout';
-
 @NgModule({
   imports: [
     CommonModule,
-    FlexLayoutModule,
   ],
   exports: [
-    FlexLayoutModule,
   ],
   declarations: []
 })

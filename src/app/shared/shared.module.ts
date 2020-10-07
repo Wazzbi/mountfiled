07a3a496c2c1eb57866d85a2d6550e0a46116348
Novotný David <danovotny@trask.cz>
2020-10-07 08:00:41 +0200

    npm fix + erease ng material modules

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,27 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { FlexLayoutModule } from '@angular/flex-layout';
 
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatIconModule} from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
-
-
 @NgModule({
   imports: [
     CommonModule,
-
     FlexLayoutModule,
-    
-    MatToolbarModule,
-    MatIconModule,
-    MatButtonModule
   ],
   exports: [
     FlexLayoutModule,
-
-    MatToolbarModule,
-    MatIconModule,
-    MatButtonModule
   ],
   declarations: []
 })
